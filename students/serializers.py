f10384c834e35219addd21c85c7e6df3db6ddfdb
Rex Makusia <grecpt@gmail.com>
2024-08-12 18:24:04 +1000

    Student_Marks_Tracker: Create Serializers with business logic

---
+++ students/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Student, Course, Marks, ProgressTracker
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class MarksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Marks
+        fields = '__all__'
+
+class ProgressTrackerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProgressTracker
+        fields = '__all__'
